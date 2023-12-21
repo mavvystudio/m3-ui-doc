ab730066248445eeb3fc94cc743c89f1662952c5
ESPIM146 <Jeric.Espiritu.-ND@disney.com>
2023-12-22 04:22:48 +0800

    chore: clean up files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,6 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -97,11 +96,10 @@ const config = {
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
             position: "left",
-            label: "Tutorial",
+            label: "Docs",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/mavvy22/m3-ui",
             label: "GitHub",
             position: "right",
           },
@@ -111,28 +109,11 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
+            title: "Site",
             items: [
               {
-                label: "Tutorial",
+                label: "Docs",
                 to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
               },
             ],
           },
@@ -140,17 +121,13 @@ const config = {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/mavvy22/m3-ui",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} MavvyStudio, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
