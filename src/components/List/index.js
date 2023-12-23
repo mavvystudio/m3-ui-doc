345618e42acf69bef6a15b69c126ac078e38cbbf
ESPIM146 <Jeric.Espiritu.-ND@disney.com>
2023-12-23 22:00:20 +0800

    add initial dialog docs

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,5 +1,8 @@
+import Preview from "../Preview";
 export default function List({children}) {
   return (
-    <div className="flex flex-col items-center gap-y-4">{children}</div>
+    <Preview>
+      <div className="flex flex-col items-center gap-y-4">{children}</div>
+    </Preview>
   );
 }
---
+++ src/components/Preview/index.js
@@ -0,0 +1,3 @@
+export default function({children}) {
+  return <div className="bg-surface-container p-[20px] rounded-lg flex justify-center">{children}</div>
+}
