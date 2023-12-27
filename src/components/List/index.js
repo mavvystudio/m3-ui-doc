f059e72f97b3f67615d3f550eab0f5eb1351cedc
ESPIM146 <Jeric.Espiritu.-ND@disney.com>
2023-12-27 20:43:50 +0800

    feat: add TextField section

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,7 +2,7 @@ import Preview from "../Preview";
 export default function List({children}) {
   return (
     <Preview>
-      <div className="flex flex-col items-center gap-y-4">{children}</div>
+      <div className="flex w-full flex-col items-center gap-y-4">{children}</div>
     </Preview>
   );
 }
